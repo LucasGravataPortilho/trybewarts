920971516583e54f9a0d210d8b79d01e99511d45
nicolas <nicolas.momog@gmail.com>
2022-09-14 19:16:36 -0300

    cabou-se

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-function login() {
+function login(event) {
+  event.preventDefault();
   const senha = '123456';
-  const coisa2 = document.querySelector('.password').value;
+  const password = document.querySelector('[name="password"]');
   const email = 'tryber@teste.com';
-  const coisa = document.querySelector('.email').value;
-  if (email === coisa && senha === coisa2) {
+  const emailZao = document.querySelector('[name="email"]');
+  if (email === emailZao.value && senha === password.value) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 }
-// login();
-console.log('oi');
 const btn = document.getElementById('enter');
 console.log(btn);
 btn.addEventListener('click', login);
