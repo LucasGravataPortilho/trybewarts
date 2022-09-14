e74ff908b66272cf299915a7df459eb7cc8cf49a
nicolas <nicolas.momog@gmail.com>
2022-09-14 15:59:10 -0300

    falta resolver o 3

---
+++ script.js
@@ -0,0 +1,14 @@
+function login() {
+  const email = 'tryber@teste.com';
+  const coisa = document.querySelector('.email').value;
+  if (email === coisa) {
+    const senha = '123456';
+    const coisa2 = document.querySelector('.password').value;
+    if (senha === coisa2) {
+      alert('Olá, Tryber!');
+    }
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+login();
